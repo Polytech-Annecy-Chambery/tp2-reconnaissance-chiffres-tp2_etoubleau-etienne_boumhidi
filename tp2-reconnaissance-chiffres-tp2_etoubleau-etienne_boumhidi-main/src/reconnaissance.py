0505b26f33c4d66fcd76c335f024a1e9a881a78a
HellOShael <90306659+HellOShael@users.noreply.github.com>
2021-11-30 11:35:56 +0100

    Add files via upload

---
+++ tp2-reconnaissance-chiffres-tp2_etoubleau-etienne_boumhidi-main/src/reconnaissance.py
@@ -0,0 +1,27 @@
+from image import Image
+
+def lecture_modeles(chemin_dossier):
+    fichiers= ['_0.png','_1.png','_2.png','_3.png','_4.png','_5.png','_6.png', 
+            '_7.png','_8.png','_9.png']
+    liste_modeles = []
+    for fichier in fichiers:
+        model = Image()
+        model.load(chemin_dossier + fichier)
+        liste_modeles.append(model)
+    return liste_modeles
+
+
+def reconnaissance_chiffre(image, liste_modeles, S):
+    bin_im = image.binarisation(S)
+    loc_im = bin_im.localisation()
+    
+    prop = 0 
+    
+    for i in range(len(liste_modeles)):
+        res=loc_im.resize(liste_modeles[i].H,liste_modeles[i].W)
+        if res.similitude(liste_modeles[i]) > prop :
+            prop = res.similitude(liste_modeles[i])
+            i_prop = i
+    return i_prop        
+            
+
